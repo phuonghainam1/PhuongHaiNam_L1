a096c7845ab52eec2c42805ed3f15f84e9537e47
Phương Nam <phuongnam@MacBook-Pro-cua-Phuong.local>
2025-06-10 21:31:29 +0700

    Push

---
+++ PhuongHaiNam_L1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PhuongHaiNam_L1.Models;
+
+namespace PhuongHaiNam_L1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ PhuongHaiNam_L1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PhuongHaiNam_L1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
